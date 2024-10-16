1f0dd2efef09bcff085a7b6f6d92a7bf4c9bf337
Rob-Gim <grafm-k@yandex.com>
2024-10-16 22:25:26 +0500

    Basa

---
+++ OsnSint_2.1.7.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>      //библиотека ввода из файла
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    ofstream fout("output.txt", ios_base::trunc);
+    fout << "a";
+    fout.close();
+
+    fstream fin;
+    fin.open("output.txt");	        //путь к файлу вывода    
+    if (fin.is_open()) {
+            fin >> s;
+            cout << s << endl;
+    }
+    fin.close();                    //закрытие файла записи
+   
+    fout.open("output.txt", ios_base::app);
+    fout << "b";
+    fout.close();
+
+    fin.open("output.txt");	        //путь к файлу вывода    
+    if (fin.is_open()) {
+            fin >> s;
+            cout << s;
+    }
+    fin.close();               //закрытие файла записи
+    fout.close();                    //закрытие файла чтения
+}
